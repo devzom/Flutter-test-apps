97eab08a631c956be744cc443e60dcd96aa66a5d
Jacob <dev.zomerfeld@gmail.com>
2020-02-01 15:17:07 +0100

    Complete Chat app with hardCoded data -> get http in future

--- symchat/lib/main.dart
+++ symchat/lib/main.dart
@@ -1,111 +1,187 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp((SymChatApp()));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SymChatApp extends StatelessWidget {
+  //add targetPlatform specific theme
+  final iOSTheme = new ThemeData(
+      primarySwatch: Colors.orange,
+      primaryColor: Colors.grey[100],
+      primaryColorBrightness: Brightness.light);
+
+  final defaultTheme = new ThemeData(
+      primarySwatch: Colors.indigo,
+      accentColor: Colors.indigo[500],
+      primaryColorBrightness: Brightness.dark);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return new MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'SymChat',
+        // check the actual platform and sets the proper theme getter from upper final spec
+        theme: defaultTargetPlatform == TargetPlatform.iOS
+            ? iOSTheme
+            : defaultTheme,
+        home: new ChatScreen());
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  ChatScreenState createState() => ChatScreenState();
+}
+
+//added MIXIN to reuse class body in multiple classes
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  // define controller for text input operations
+  final TextEditingController _textController = new TextEditingController();
+
+  // init a empty list of message when there's none
+  final List<ChatMessage> _message = <ChatMessage>[];
+
+  //make SEND button context-aware and change it's appearance due to state of inputTextField whether is empty or not
+  bool _isComposing = false;
+
+  //add animation contoller
+  @override
+  void dispose() {
+    for (ChatMessage message in _message) message.animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('SymChat'),
+        centerTitle: true,
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 1.0 : 6.0,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Column(
+        children: <Widget>[
+          new Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _message[index],
+              itemCount: _message.length,
+            ),
+          ),
+          Divider(height: 1),
+          Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer()),
+        ],
+      ),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+        child: Row(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Flexible(
+              child: new TextField(
+                controller: _textController,
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    InputDecoration.collapsed(hintText: "How we can help You?"),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 6.0),
+              child: IconButton(
+                  icon: Icon(Icons.arrow_forward_ios),
+                  onPressed: _isComposing
+                      ? () => _handleSubmitted(_textController.text)
+                      : null),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+
+    ChatMessage message = new ChatMessage(
+      text: text,
+      animationController: AnimationController(
+          vsync: this, duration: Duration(milliseconds: 500)),
+    );
+
+    setState(() {
+      _message.insert(0, message);
+    });
+
+    //specifing that animation should play forward only
+    message.animationController.forward();
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.animationController});
+  final String text;
+
+  //add a member to store animation controller
+  final AnimationController animationController;
+
+  static const String _name = "Jacob";
+
+  @override
+  Widget build(BuildContext context) {
+    var dateTime = DateTime.now();
+    return SizeTransition(
+      sizeFactor:
+          CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      child: Container(
+          margin: EdgeInsets.symmetric(vertical: 12),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(right: 12),
+                child: CircleAvatar(
+                  child: Text(
+                    _name[0],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(_name,
+                            style: Theme.of(context).textTheme.subtitle),
+                        Text(DateFormat('kk:mm').format(dateTime),
+                            style: TextStyle(fontSize: 8)),
+                      ],
+                    ),
+                    Container(
+                        margin: EdgeInsets.only(top: 5), child: Text(text)),
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
